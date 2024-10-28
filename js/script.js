3d11a73de170f27366a8e8dd607b559934c30f2d
lucaconigliaro <lucaconigliaro1@gmail.com>
2024-10-28 13:40:16 +0100

    feat: Km and User Age

---
+++ js/script.js
@@ -0,0 +1,9 @@
+//Chilometri da percorrere
+const userKm = prompt ("Quanto è lungo il tuo viaggio?");
+const userKmAsInt = parseInt (userKm);
+console.log (userKmAsInt);
+
+//Età dell'utente
+const userAge = prompt ("Quanti anni hai?");
+const userAgeAsInt = parseInt (userAge);
+console.log (userAgeAsInt);
